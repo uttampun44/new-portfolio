a4a069576fd078bfad6f167b1710936736de93f5
uttam pun <uttampun50@gmail.com>
2025-07-09 13:24:32 +0545

    layout import fix

--- app/layout/layout.tsx
+++ app/layout/layout.tsx
@@ -1,5 +1,5 @@
-import Footer from "@/components/footer";
-import Header from "@/components/header";
+import Footer from "@components/footer";
+import Header from "@components/header";
 import React from "react";
 
 type props = {
