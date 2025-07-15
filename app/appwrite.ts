aa7a14d2952c5b4672b6f54fde55e06ee1a8c865
uttam pun <uttampun50@gmail.com>
2025-07-15 10:33:24 +0545

    type check

--- app/appwrite.ts
+++ app/appwrite.ts
@@ -1,12 +1,14 @@
 "use client";
 
-import { Client, Account } from 'appwrite';
+import { Client, Account } from "appwrite";
 
 export const client = new Client();
 
-client
-    .setEndpoint(process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT ?? '')
-    .setProject(process.env.NEXT_PUBLIC_APPWRITE_PROJECT_ID ?? '');
+if (typeof window !== "undefined") {
+  client
+    .setEndpoint(process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT as string)
+    .setProject(process.env.NEXT_PUBLIC_APPWRITE_PROJECT_ID as string);
+}
 
 export const account = new Account(client);
-export { ID } from 'appwrite';
+export { ID } from "appwrite";
