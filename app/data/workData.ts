19b02629447cd774f8851d55c8427a970e05a1ab
uttam pun <uttampun50@gmail.com>
2025-07-16 16:01:10 +0545

    added new jobs details

--- app/data/workData.ts
+++ app/data/workData.ts
@@ -1,10 +1,35 @@
 export const workData = [
-    {
-        title: "Bright Software / Software Engineer",
-        date: "11 January 2024 - 1 July 2025, Butwal Rupandehi",
-        descriptionOne: "I have been working as a Software Engineer at Bright Software for the past 1.5. During this time, I have gained extensive experience in developing robust and scalable web applications using React, Laravel, Mysql, and React Native. I have also been involved in various projects, including building a custom CMS for a client's Bright CMS platform and contributing to the development of a Website.",
-        descriptionTwo: "I have a strong background in JavaScript, React, and Laravel, and have experience working with various frameworks and libraries. I am proficient in TypeScript and have a solid understanding of the React component lifecycle. I am also familiar with Git and GitHub, and have experience using GitHub Actions for continuous integration and deployment.",
-        techonologies: ["React", "Laravel", "TypeScript", "Git", "React Native"]
-    },
-    
-]
+  {
+    title: "Bright Software / Software Engineer",
+    date: "11 January 2024 - 1 July 2025, Butwal Rupandehi",
+    descriptionOne:
+      "I have been working as a Software Engineer at Bright Software for the past 1.5. During this time, I have gained extensive experience in developing robust and scalable web applications using React, Laravel, Mysql, and React Native. I have also been involved in various projects, including building a custom CMS for a client's Bright CMS platform and contributing to the development of a Website.",
+    descriptionTwo:
+      "I have a strong background in JavaScript, React, and Laravel, and have experience working with various frameworks and libraries. I am proficient in TypeScript and have a solid understanding of the React component lifecycle. I am also familiar with Git and GitHub, and have experience using GitHub Actions for continuous integration and deployment.",
+    techonologies: ["React", "Laravel", "TypeScript", "Git", "React Native"],
+  },
+  {
+    title: "Frontend Developer – PCsoft Nepal",
+    date: "May 22, 2022 – January 11, 2024",
+    descriptionOne:
+      "During my time at PCsoft Nepal, I worked as a Frontend Developer for 1.5 years, focusing on the development of dynamic and responsive web applications. My work involved using React, Laravel, MySQL, and React Native to deliver efficient and scalable solutions tailored to client needs.",
+    descriptionTwo:
+      "I built a strong foundation in Wordpress, Php, Javascript, UI designs.",
+    techonologies: ["WordPress", "JavaScript", "PHP", "Git", "Figma"],
+  },
+  {
+    title: "Computer Teacher",
+    date: "February 20, 2021 – December 2021",
+    descriptionOne:
+      "During my tenure as a Computer Teacher, I was responsible for instructing students in the fundamentals of computer operations, Microsoft Office applications, and basic web development. I created lesson plans, guided practical sessions, and helped students build a strong foundation in computer literacy and digital skills.",
+    descriptionTwo:
+      "This role strengthened my communication and teaching abilities while also allowing me to deepen my understanding of core technologies like HTML, CSS, and JavaScript. I also introduced students to WordPress for basic website development and ensured they were confident in using productivity tools like Microsoft Word, Excel, and PowerPoint.",
+    techonologies: [
+      "Microsoft Office",
+      "WordPress",
+      "HTML",
+      "CSS",
+      "JavaScript",
+    ],
+  },
+];
