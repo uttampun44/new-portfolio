b39d084aaca5069c58b846fcd5320c6b74e97e48
uttam pun <uttampun50@gmail.com>
2025-07-16 16:39:11 +0545

    add undefined

--- app/appwrite.ts
+++ app/appwrite.ts
@@ -4,9 +4,11 @@ import { Client, Account } from "appwrite";
 
 export const client = new Client();
 
+if (typeof window !== "undefined") {
   client
     .setEndpoint(process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT as string)
     .setProject(process.env.NEXT_PUBLIC_APPWRITE_PROJECT_ID as string);
+}
 
 export const account = new Account(client);
 export { ID } from "appwrite";
